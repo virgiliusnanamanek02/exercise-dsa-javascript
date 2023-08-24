14edf39ec5d1f469846d4e4ea074675dd8595e45
Vicky Nana <virgilius.nana.manek0719@gmail.com>
2023-08-24 21:36:04 +0700

    basic array

---
+++ array/basic-array.js
@@ -0,0 +1,48 @@
+// Array Data Structure
+
+class Array {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.data;
+  }
+
+  pop() {
+    // remove last item and return it
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+    return item;
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+const arr = new Array();
+arr.push(1);
+arr.push(2);
+arr.push(3);
+console.log(arr);
+
+console.log(arr.pop());
